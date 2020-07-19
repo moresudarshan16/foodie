378d87b6e0fbc0cd56d0b51fecc9d400f76f9e38
sudarshan <sudarshan@gmail.com>
2020-07-19 19:55:48 +0530

    new

---
+++ webdesign/data.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'connection.php';
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$emailid = $_POST['emailid'];
+$contactno = $_POST['contactno'];
+$address = $_POST['address'];
+
+$sql = "INSERT INTO `contact_us` (`name`,`subject`,`message`,`emailid`,`contactno`,`address`) VALUES('$name','$subject','$message','$emailid','$contactno','$address')";
+
+$result = mysqli_query($conn,$sql);
+  
+if ($result== TRUE){
+$success = 'success';
+$_SESSION['success']= $success;
+header("Location: contactus.html");
+}
+
+
+?>
